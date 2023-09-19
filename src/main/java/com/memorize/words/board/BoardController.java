10ee732f9d01563122a78a72dca377b583a736b8
back-seung <skud113@naver.com>
2023-09-19 19:39:55 +0900

    유저 엔티티 생성

--- src/main/java/com/memorize/words/board/BoardController.java
+++
@@ -1,1 +0,0 @@
-package com.memorize.words.board;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;@Controllerpublic class BoardController {    @GetMapping("/")    public String list() {        return "list";    }}
